15b33359cfb9da0bc4edcbcad609f763614b2313
Ivan Puljak <ivan@puljak.ca>
2018-11-30 22:26:43 -0500

    Started BinarySearchTree implementation

---
+++ data_structures/BinarySearchTree.go
@@ -0,0 +1,17 @@
+package main
+
+// Node struct
+type Node struct {
+	value int
+	left  *Node
+	right *Node
+}
+
+// BinarySearchTree struct
+type BinarySearchTree struct {
+	root *Node
+}
+
+func main() {
+
+}
