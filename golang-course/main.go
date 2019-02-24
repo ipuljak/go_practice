6f6192a6ad26e145f1a6cc4fd9b8d781cf07dfeb
ipuljak <ivan@puljak.ca>
2019-02-24 18:08:02 -0500

    Maps in go

--- golang-course/main.go
+++ golang-course/main.go
@@ -2,53 +2,34 @@ package main
 
 import "fmt"
 
+// Income struct
+type Income struct {
+	salary int
+}
+
+// Person struct
+type Person struct {
+	first  string
+	last   string
+	age    int
+	income Income
+}
+
 func main() {
-	// Create slices
-	x := []int{4, 5, 7, 8, 42}
-	y := []int{0, 1, 2}
-
-	fmt.Println(x[1])
-
-	// Loop over slice
-	for i, v := range x[1:3] {
-		fmt.Println(i, v)
+	John := Person{
+		first:  "John",
+		last:   "Smith",
+		age:    30,
+		income: Income{salary: 50000},
 	}
 
-	// Merge slices
-	x = append(x, y...)
-
-	// Delete from slice
-	x = append(x[:2], x[4:]...)
-
-	fmt.Println(x)
-
-	// "make" slices
-	a := make([]int, 10, 100)
-
-	fmt.Println(a)
-
-	// Maps
-
-	m := map[string]int{
-		"Ivan":  27,
-		"Holly": 25,
+	Bob := Person{
+		first:  "Bob",
+		last:   "Dole",
+		age:    30,
+		income: Income{salary: 50000},
 	}
 
-	// Checking if something exists in a map
-	if _, ok := m["Steve"]; ok {
-		fmt.Println("Steve exists")
-	} else {
-		fmt.Println("Steve does not exist")
-	}
-
-	// Add elements + range in map
-	m["Steve"] = 58
-
-	for k, v := range m {
-		fmt.Println(k, v)
-	}
-
-	// Delete map
-	delete(m, "Steve")
-	fmt.Println(m)
+	fmt.Println(John, Bob)
+	fmt.Println(John.first)
 }
