ebe950261b4ff7a5c62ae58697ff575f21b4f01c
ipuljak <ivan@puljak.ca>
2019-02-27 19:51:22 -0500

    Go context

--- golang-course/channels.go
+++ golang-course/channels.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"math/rand"
+	"runtime"
 	"sync"
 	"time"
 )
@@ -277,6 +279,42 @@ func timeConsumingWork(n int) int {
 	return n + rand.Intn(1000)
 }
 
+// Context - makes sure that a process which launches a whole lot of goroutines
+// If you cancel that process, it should cancel all of the launched goroutines as well
+// Prevents from goroutines leaking out
+
+func mainContext() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	fmt.Println("Error check 1: ", ctx.Err())
+	fmt.Println("Num goroutines 1: ", runtime.NumGoroutine())
+
+	go func() {
+		n := 0
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				n++
+				time.Sleep(time.Millisecond * 200)
+				fmt.Println("working", n)
+			}
+		}
+	}()
+
+	time.Sleep(time.Second * 2)
+	fmt.Println("Error check 2: ", ctx.Err())
+	fmt.Println("Num goroutines 2: ", runtime.NumGoroutine())
+
+	fmt.Println("About to cancel context...")
+	cancel()
+
+	time.Sleep(time.Second * 2)
+	fmt.Println("Error check 3: ", ctx.Err())
+	fmt.Println("Num goroutines 3: ", runtime.NumGoroutine())
+}
+
 func mainChannels() {
-	fanOutChannels()
+	mainContext()
 }
